7dd3f194ce213d4d3ae8486d286cc996566fe9d1
Cmbailey089 <christopherbailey089@gmail.com>
2023-03-27 19:01:01 -0400

    added a template for readME

--- develop/index.js
+++ develop/index.js
@@ -1,64 +1,111 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const {writeReadMe} = require('fs').promises;
+const { writeFile } = require('fs').promises;
 
 // TODO: Create an array of questions for user input
-const questions = () => {
-    return inquirer.prompt([
+const questions = ({title,description,installation}) =>
+`(https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+
+#${title}
+
+##A quick description of the app
+
+${description}
+
+#Table of Contents
+
+*[installation](#installation)
+
+*[usage](#usage)
+
+*[license](#license)
+
+*[contribution](#contribution)
+
+*[test](#test)
+
+*[github](#github)
+
+*[email](#email)
+
+${installation}
+${usage}
+${license}
+${contribution}
+${usage}
+${github}
+${email}
+
+
+`
+    inquirer.prompt([
         {
             type:'input',
-            name: 'name',
-            messages: 'message',
+            name: 'title',
+            messages: 'What is the title of your project?',
         },
         {
             type:'input',
-            name: 'name',
-            messages: 'message',
+            name: 'description',
+            messages: 'Describe your project.',
         },
         {
             type:'input',
-            name: 'name',
-            messages: 'message',
+            name: 'installation',
+            messages: 'What do you install to create your app?',
         },
         {
             type:'input',
-            name: 'name',
-            messages: 'message',
+            name: 'usage',
+            messages: 'How do you use your app?',
         },
         {
             type:'input',
-            name: 'name',
-            messages: 'message',
+            name: 'license',
+            messages: 'Which license do you use?',
         },
         {
             type:'input',
-            name: 'name',
-            messages: 'message',
+            name: 'contribution',
+            messages: 'How can contributions be made?',
         },
         {
             type:'input',
-            name: 'name',
-            messages: 'message',
+            name: 'test',
+            messages: 'How do you test your app?',
         },
         {
             type:'input',
-            name: 'name',
-            messages: 'message',
+            name: 'github',
+            messages: 'Enter your Github Username.',
         },
         {
             type:'input',
-            name: 'name',
-            messages: 'message',
+            name: 'email',
+            messages: 'Enter your email address.',
         },
 
 ]);
-};
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile (fileName, data) {
+    fs.writeFile(fileName,data, function(err) {
+        console.log("yes")
+        if (err) {
+            return console.log(err)
+        } else {
+            console.log("done")
+        }
+    })
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+}
 
 // Function call to initialize app
 init();
